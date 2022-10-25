d3902c819a995194732df1137eae238933e2becd
Nate Anderson <nja5387@gmail.com>
2022-10-25 17:45:56 -0400

    added constaint for Iphone notch

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>AIO Health, your all in one health app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
+    
   );
 }
 
@@ -16,6 +19,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
